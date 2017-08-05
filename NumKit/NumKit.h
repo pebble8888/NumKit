7d8806e6cd46d11a6d65f3f413a9bd4adb4ff196
pebble8888 <pebble8888@gmail.com>
2017-08-05 20:31:10 +0900

    Initial Commit

---
+++ NumKit/NumKit.h
@@ -0,0 +1,19 @@
+//
+//  NumKit.h
+//  NumKit
+//
+//  Created by pebble8888 on 2017/08/05.
+//  Copyright © 2017年 pebble8888. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NumKit.
+FOUNDATION_EXPORT double NumKitVersionNumber;
+
+//! Project version string for NumKit.
+FOUNDATION_EXPORT const unsigned char NumKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NumKit/PublicHeader.h>
+
+
